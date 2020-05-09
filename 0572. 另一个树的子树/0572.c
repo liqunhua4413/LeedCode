95e116ca17aa15cece0988797635c8cc1e471c62
zero <33324180+liqunhua4413@users.noreply.github.com>
2020-05-09 20:10:36 +0800

    Update 0572.c

--- 0572. 另一个树的子树/0572.c
+++ 0572. 另一个树的子树/0572.c
@@ -8,11 +8,13 @@
  */
 
 /*
+周末更新：
+出错地方在下面指出了，递归时居然忘了给res赋值。
 思路：该方法一直报错，等周天统一调一下。
 首先找到val相同的节点，找这个节点，只需遍历s的值是否有跟子树头节点值一样的即可。
 然后比较后面的值是否相同，当到达NULL值时，还保持相同则是子树，否则不是。
 */
-/*
+
 bool DoesTree1HasTree2(struct TreeNode* t1, struct TreeNode* t2){ //比较两棵树后面的值是否相同。
     bool resLeft = false;
     bool resRight = false; //左右节点初始都设置为false即不同。
@@ -32,18 +34,22 @@ bool isSubtree(struct TreeNode* s, struct TreeNode* t){
     if(s && t){ //保证两个二叉树非空，考虑鲁棒性。同时保证，如果下面递归isSubtree的时候，如果s已经为空，返回res的值即false。
         if(s->val == t->val) res = DoesTree1HasTree2(s, t); //当两个val相同时，判断树后面的值是否相同。
         //下面的思想很关键，就是当结果res为假，没有找到子树时，才允许继续找。一旦找到了就直接返回true了。
-        if(!res) isSubtree(s->left, t);
-        if(!res) isSubtree(s->right, t);        
+        if(!res) res = isSubtree(s->left, t); //开始没有给res赋值，导致结果一直输出false，不对。
+        if(!res) res = isSubtree(s->right, t); //开始没有给res赋值，导致结果一直输出false，不对。       
     }
     return res;
 }
-*/
+
+
+
 
 /*
+更新：本题重新做了一遍，故更新一下。
 先参考此人答案提交一波：
 作者：xu-du-guang-yin-2
 链接：https://leetcode-cn.com/problems/subtree-of-another-tree/solution/di-gui-suan-fa-jie-jue-zi-shu-wen-ti-by-xu-du-guan/
 */
+/*
 bool compare (struct TreeNode* p1,struct TreeNode* p2)
 {
     if(!p1&&!p2) return true;
@@ -55,4 +61,4 @@ bool isSubtree(struct TreeNode* s, struct TreeNode* t){
     if(!s) return false;
     return compare(s,t)||isSubtree(s->left,t)||isSubtree(s->right,t);
 }
-
+*/
